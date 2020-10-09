9ab32951029a455e0d049b45bbf9e750c24ba81e
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2020-10-08 20:37:34 -0400

    implementing get requests

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const { response } = require('express');
 const express = require('express')
 const app = express();
-app.listen(3009, ()=> console.log('server now listening at port 3000'))
+const fs = require('fs');
+
+const products = JSON.parse(fs.readFileSync('products.json'));
+
+console.log(products);
+console.log(products[0].Source)
+
+app.get('/', (requ,resp)=>{ resp.send('Hi !! you got something!'); } );
+app.get('/products', (requ,resp)=>{  
+    resp.send(products);
+    console.log(requ.query);
+});
+app.get('/products/:id',(requ,resp)=>{  
+    console.log(requ.params);
+    resp.send("hello "+requ.params.id);
+ }   );
+
+app.listen(3009, ()=> console.log('server now listening at port 3009'))
 
 
