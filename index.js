233eefa3d3281debab5dbd385f79a167cf5e394b
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2020-10-08 21:02:37 -0400

    implemented and cleaned up get request by ID

--- index.js
+++ index.js
@@ -5,24 +5,23 @@ const fs = require('fs');
 const products = JSON.parse(fs.readFileSync('products.json'));
 
 console.log(products);
-console.log(products[0].Source)
+console.log(products[0].Source);
 
 app.get('/products', (requ,resp)=>{  
     resp.send(products);
 });
+
 app.get('/products/:ID',(requ,resp)=>{  
     console.log(requ.params);
+    var userProduct = "not found :(";
 
     for(var product in products) {
         if (products[product].ID == requ.params.ID) {
-            resp.send("here's your product !!\n"+products[product]);
+            userProduct = products[product];
         }
-        else{
-            console.log("sorry :( couldn't find your product");
-        }
-
      }
- }   );
+     resp.send(JSON.stringify(userProduct, null, 2));
+ });
 
 app.listen(3009, ()=> console.log('server now listening at port 3009'))
 
